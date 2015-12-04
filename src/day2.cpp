86a52a1595b2f897a5ec3c3d12cd3fcf60012087
Will Killian <william.killian@gmail.com>
2015-12-03 22:16:53 -0500

    Changed split to be more efficient -- again :)

--- src/day2.cpp
+++ src/day2.cpp
@@ -8,10 +8,10 @@ using box = std::array <int, 3>;
 
 box
 parseLine (std::string line) {
+	box b;
 	std::replace (std::begin (line), std::end (line), 'x', ' ');
 	std::istringstream iss { line };
-	box b;
-	iss >> b[0] >> b[1] >> b[2];
+	std::copy (std::istream_iterator <int> {iss}, std::istream_iterator <int> {}, std::begin (b));
 	return b;
 }
 
@@ -25,9 +25,9 @@ main (int argc, char* argv[]) {
 		box b = parseLine (line);
 		std::sort (std::begin (b), std::end (b));
 		if (!part2) {
-			total += 3 * b[0] * b[1] + 2 * b[2] * (b[0] + b[1]);
+			total += 3 * (b[0] * b[1]) + 2 * b[2] * (b[0] + b[1]);
 		} else {
-			total += 2 * (b[0] + b[1]) + b[0] * b[1] * b[2];
+			total += 2 * (b[0] + b[1]) + (b[0] * b[1] * b[2]);
 		}
 	}
 	std::cout << total << std::endl;
