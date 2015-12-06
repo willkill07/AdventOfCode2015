00397185ebd6a1cc3638c214a9d67b057786fbb8
Will Killian <william.killian@gmail.com>
2015-12-06 11:28:02 -0500

    Fixed spacing for day 5

--- src/day5.cpp
+++ src/day5.cpp
@@ -11,15 +11,15 @@ std::regex post ("(.).\\1");
 
 bool
 isNice (const std::string& str) {
-	return std::regex_search (str, threeVowels) &&
-		std::regex_search (str, doubleLetter) &&
-		!std::regex_search (str, bad);
+  return std::regex_search (str, threeVowels) &&
+    std::regex_search (str, doubleLetter) &&
+    !std::regex_search (str, bad);
 }
 
 bool
 isNice2 (const std::string& str) {
-	return std::regex_search (str, pair) &&
-		std::regex_search (str, post);
+  return std::regex_search (str, pair) &&
+    std::regex_search (str, post);
 }
 
 
@@ -27,14 +27,14 @@ int
 main (int argc, char* argv []) {
   bool part2 { argc == 2 };
 
-	int niceCount { 0 };
+  int niceCount { 0 };
 
   std::string input;
-	while (std::getline (std::cin, input)) {
-		if ((!part2 && isNice (input)) || (part2 && isNice2 (input))) {
-			++niceCount;
-		}
-	}
+  while (std::getline (std::cin, input)) {
+    if ((!part2 && isNice (input)) || (part2 && isNice2 (input))) {
+      ++niceCount;
+    }
+  }
   std::cout << niceCount << std::endl;
   return 0;
 }
