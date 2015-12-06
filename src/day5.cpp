3ae7852c30e79d540e321f0c33ee9115be3eec0f
Will Killian <william.killian@gmail.com>
2015-12-05 23:00:10 -0500

    Day 5

---
+++ src/day5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+std::regex threeVowels ("([aeiou].*){3,}");
+std::regex doubleLetter ("(.)\\1");
+std::regex bad ("(ab|cd|pq|xy)");
+
+std::regex pair ("(..).*\\1");
+std::regex post ("(.).\\1");
+
+bool
+isNice (const std::string& str) {
+  return std::regex_search (str, threeVowels) &&
+    std::regex_search (str, doubleLetter) &&
+    !std::regex_search (str, bad);
+}
+
+bool
+isNice2 (const std::string& str) {
+  return std::regex_search (str, pair) &&
+    std::regex_search (str, post);
+}
+
+
+int
+main (int argc, char* argv []) {
+  bool part2 { argc == 2 };
+
+  int niceCount { 0 };
+
+  std::string input;
+  while (std::getline (std::cin, input)) {
+    if ((!part2 && isNice (input)) || (part2 && isNice2 (input))) {
+      ++niceCount;
+    }
+  }
+  std::cout << niceCount << std::endl;
+  return 0;
+}
