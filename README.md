c962e3b08f3a764351984a61662ade3343bb2f9b
Will Killian <william.killian@gmail.com>
2015-12-08 18:58:42 -0500

    Updated day7 to make more sense

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Implementations are written in C++(11/14)
 
 Day | Lines | Source Lines | Total Bytes | Non WS Bytes | Time | Source
 ----|-------|--------------|-------------|--------------|------|-------
-1 | 19 | 18 | 378 | 264 | 0.0037 | [day1.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day1.cpp)
-2 | 30 | 26 | 810 | 643 | 0.0054 | [day2.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day2.cpp)
-3 | 39 | 36 | 921 | 683 | 0.0072 | [day3.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day3.cpp)
-4 | 21 | 20 | 504 | 387 | 0.4174 | [day4.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day4.cpp)
-5 | 24 | 22 | 771 | 638 | 0.0586 | [day5.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day5.cpp)
-6 | 60 | 55 | 1873 | 1443 | 0.5054 | [day6.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day6.cpp)
-7 | 76 | 72 | 2295 | 1664 | 0.0058 | [day7.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day7.cpp)
-8 | 27 | 24 | 898 | 727 | 0.0062 | [day8.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day8.cpp)
-**TOTAL** | **296** | **273** | **8450** | **6449** | **1.0097** |
+1 | 19 | 18 | 378 | 264 | 0.0032 | [day1.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day1.cpp)
+2 | 30 | 26 | 810 | 643 | 0.0053 | [day2.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day2.cpp)
+3 | 39 | 36 | 921 | 683 | 0.0076 | [day3.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day3.cpp)
+4 | 21 | 20 | 504 | 387 | 0.4192 | [day4.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day4.cpp)
+5 | 24 | 22 | 771 | 638 | 0.0556 | [day5.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day5.cpp)
+6 | 60 | 55 | 1873 | 1443 | 0.5306 | [day6.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day6.cpp)
+7 | 80 | 72 | 2297 | 1664 | 0.0064 | [day7.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day7.cpp)
+8 | 27 | 24 | 898 | 727 | 0.0057 | [day8.cpp](https://github.com/willkill07/adventofcode/blob/master/src/day8.cpp)
+**TOTAL** | **300** | **273** | **8452** | **6449** | **1.0336** |
--- src/day7.cpp
+++ src/day7.cpp
@@ -10,68 +10,71 @@ static const std::regex BINARY_OP { "(\\w+) (AND|OR|LSHIFT|RSHIFT) (\\w+) -> (\\
 
 using Int = std::uint16_t;
 using Callback = std::function <Int()>;
-struct Data {
+
+struct Gate {
   Callback fn;
   std::string val1, val2;
   bool memoized { false };
   Int value;
-  Data (std::string v1, std::string v2, Callback f) : fn { f }, val1 { v1 }, val2 { v2 } { }
-  Data (std::string v1, Callback f) : Data (v1, { }, f) { };
-  Data (Int val) : memoized { true }, value { val } { };
+  Gate (std::string v1, std::string v2, Callback f) : fn { f }, val1 { v1 }, val2 { v2 } { }
+  Gate (std::string v1, Callback f) : Gate (v1, { }, f) { };
+  Gate (Int val) : memoized { true }, value { val } { };
 };
-using Eval = std::map <std::string, Data>;
 
-Eval lookup;
+struct Circuit {
+  std::map <std::string, Gate> lookup;
 
-void set (std::string key, Int v) {
-  lookup.at (key) = Data { v };
-}
+	void set (std::string key, Int v) {
+    lookup.at (key) = Gate { v };
+  }
 
-Int get (std::string value) {
-  try {
-    return std::stoi (value);
-  } catch (...) {
-    auto & d = lookup.at (value);
-    if (d.memoized)
+	Int get (std::string value) {
+    try {
+      return std::stoi (value);
+    } catch (...) {
+      auto & d = lookup.at (value);
+      if (d.memoized)
+        return d.value;
+      d.value = d.fn();
+      d.memoized = true;
       return d.value;
-    d.value = d.fn();
-    d.memoized = true;
-    return d.value;
+    }
   }
-}
 
-void parseLine (std::string line, Eval &l) {
-  std::smatch m;
-  if (std::regex_match (line, m, ASSIGN_OP)) {
-    std::string out { m [2] };
-    l.emplace (out, Data { m [1], [out, &l] () {
-      return get (l.at (out).val1);
-    } });
-  } else if (std::regex_match (line, m, NOT_OP)) {
-    std::string out { m [2] };
-    l.emplace (out, Data { m [1], [out, &l] () {
-      return ~get (l.at (out).val1);
-    } });
-  } else if (std::regex_match (line, m, BINARY_OP)) {
-    std::string op { m [2] }, out { m [4] };
-    l.emplace (out, Data { m [1], m [3], [out, op, &l] () {
-      Data & d { l.at (out) };
-      return ((op.compare ("AND") == 0) ? (get (d.val1) & get (d.val2)) :
-       ((op.compare ("OR") == 0) ? (get (d.val1) | get (d.val2)) :
-        ((op.compare ("LSHIFT") == 0) ? (get (d.val1) << get (d.val2)) :
-         ((get (d.val1) >> get (d.val2))))));
-    } });
+  void parseLine (std::string line) {
+    std::smatch m;
+    if (std::regex_match (line, m, ASSIGN_OP)) {
+      std::string out { m [2] };
+      lookup.emplace (out, Gate { m [1], [&, out] () {
+        return get (lookup.at (out).val1);
+      } });
+    } else if (std::regex_match (line, m, NOT_OP)) {
+      std::string out { m [2] };
+      lookup.emplace (out, Gate { m [1], [&, out] () {
+        return ~get (lookup.at (out).val1);
+      } });
+    } else if (std::regex_match (line, m, BINARY_OP)) {
+      std::string op { m [2] }, out { m [4] };
+      lookup.emplace (out, Gate { m [1], m [3], [&, out, op] () {
+        Gate & g { lookup.at (out) };
+        return ((op.compare ("AND") == 0) ? (get (g.val1) & get (g.val2)) :
+                ((op.compare ("OR") == 0) ? (get (g.val1) | get (g.val2)) :
+                 ((op.compare ("LSHIFT") == 0) ? (get (g.val1) << get (g.val2)) :
+                  ((get (g.val1) >> get (g.val2))))));
+      } });
+    }
   }
-}
+};
 
 int main (int argc, char* argv []) {
   Timer t;
   bool part2 { argc == 2 };
+  Circuit c;
   std::string line;
   while (std::getline (std::cin, line))
-    parseLine (line, lookup);
+    c.parseLine (line);
   if (part2)
-    set ("b", 956);
-  std::cout << get ("a") << std::endl;
+    c.set ("b", 956);
+  std::cout << c.get ("a") << std::endl;
   return 0;
 }
