93240578c571772767e021c18213da287155b611
Will Killian <william.killian@gmail.com>
2015-12-07 20:08:40 -0500

    optimized parsing

--- src/day5.cpp
+++ src/day5.cpp
@@ -3,23 +3,11 @@
 #include <regex>
 #include "timer.hpp"
 
-std::regex threeVowels ("([aeiou].*){3,}");
-std::regex doubleLetter ("(.)\\1");
-std::regex bad ("(ab|cd|pq|xy)");
-
-std::regex pair ("(..).*\\1");
-std::regex post ("(.).\\1");
-
-bool
-isNice (const std::string& str) {
-  return std::regex_search (str, threeVowels) && std::regex_search (str, doubleLetter) && !std::regex_search (str, bad);
-}
-
-bool
-isNice2 (const std::string& str) {
-  return std::regex_search (str, pair) && std::regex_search (str, post);
-}
-
+const static std::regex threeVowels { "([aeiou].*){3,}" };
+const static std::regex doubleLetter { "(.)\\1" };
+const static std::regex bad { "(ab|cd|pq|xy)" };
+const static std::regex pair { "(..).*\\1" };
+const static std::regex post { "(.).\\1" };
 
 int
 main (int argc, char* argv []) {
@@ -27,9 +15,10 @@ main (int argc, char* argv []) {
   bool part2 { argc == 2 };
   int niceCount { 0 };
 
-  std::string input;
-  while (std::getline (std::cin, input)) {
-    if ((!part2 && isNice (input)) || (part2 && isNice2 (input))) {
+  std::string str;
+  while (std::getline (std::cin, str)) {
+    if ((!part2 && std::regex_search (str, threeVowels) && std::regex_search (str, doubleLetter) && !std::regex_search (str, bad)) ||
+        (part2 && std::regex_search (str, pair) && std::regex_search (str, post))) {
       ++niceCount;
     }
   }
