3fe89868e6212d84882be9a74326e2b9eed97ab6
Will Killian <william.killian@gmail.com>
2015-12-08 19:50:49 -0500

    golfing on problem 2

--- src/day2.cpp
+++ src/day2.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <array>
 #include <iostream>
 #include <regex>
 #include <string>
@@ -7,23 +6,17 @@
 
 static const std::regex PARSE { "(\\d+)x(\\d+)x(\\d+)" };
 
-using Box = std::array <int, 3>;
-
-void parseLine (std::string line, Box &b) {
-  std::smatch m;
-  std::regex_search (line, m, PARSE);
-	std::transform (++m.begin(), m.end(), b.begin(), [] (const auto &o) { return std::stoi (o); });
-	std::sort (b.begin(), b.end());
-}
-
 int main (int argc, char* argv []) {
   Timer t;
   bool part2 { argc == 2 };
   int total { 0 };
-	Box b;
+	int b[3];
+	std::smatch m;
 	std::string line;
   while (std::getline (std::cin, line)) {
-		parseLine (line, b);
+		std::regex_search (line, m, PARSE);
+		std::transform (++m.begin(), m.end(), b.begin(), [] (const auto &o) { return std::stoi (o); });
+		std::sort (b.begin(), b.end());
     total += ((part2) ? (2 * (b[0] + b[1]) + (b[0] * b[1] * b[2])) : (3 * (b[0] * b[1]) + 2 * b[2] * (b[0] + b[1])));
   }
   std::cout << total << std::endl;
