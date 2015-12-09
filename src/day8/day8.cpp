fec66cf003ea0b4f7ccc2c87b70d0b64340c0a9a
Will Killian <william.killian@gmail.com>
2015-12-09 13:13:16 -0500

    whitespace

--- src/day8/day8.cpp
+++ src/day8/day8.cpp
@@ -8,12 +8,12 @@
 static const std::regex REDUCE { R"(\\(\\|\"|x[0-9a-f]{2}))" }, EXPAND { R"(\"|\\)" };
 
 auto fn (bool part2) -> std::function <int (int, const std::string & s)> {
-	if (part2) return [] (int c, const auto &s) { return c + std::accumulate (std::sregex_iterator { s.begin(), s.end(), REDUCE }, { }, 2, [](int v, auto &m) -> int { return v + m.length() - 1; }); };
-	else return [] (int c, const auto &s) { return c + 2 + std::distance (std::sregex_iterator { s.begin(), s.end(), EXPAND }, { }); };
+  if (part2) return [] (int c, const auto &s) { return c + std::accumulate (std::sregex_iterator { s.begin(), s.end(), REDUCE }, { }, 2, [](int v, auto &m) -> int { return v + m.length() - 1; }); };
+  else return [] (int c, const auto &s) { return c + 2 + std::distance (std::sregex_iterator { s.begin(), s.end(), EXPAND }, { }); };
 }
 
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-	std::cout << std::accumulate (std::istream_iterator <std::string> { std::cin }, { }, 0, fn (part2)) << std::endl;
+  std::cout << std::accumulate (std::istream_iterator <std::string> { std::cin }, { }, 0, fn (part2)) << std::endl;
   return 0;
 }
