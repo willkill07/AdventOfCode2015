b57fdbedcadeaaa48874c64abe9cedf51afe45dc
Will Killian <william.killian@gmail.com>
2015-12-08 19:52:19 -0500

    golfing on problem 2

--- src/day2.cpp
+++ src/day2.cpp
@@ -10,13 +10,13 @@ int main (int argc, char* argv []) {
   Timer t;
   bool part2 { argc == 2 };
   int total { 0 };
-	int b[3];
-	std::smatch m;
-	std::string line;
+  int b[3];
+  std::smatch m;
+  std::string line;
   while (std::getline (std::cin, line)) {
-		std::regex_search (line, m, PARSE);
-		std::transform (++m.begin(), m.end(), b, [] (const auto &o) { return std::stoi (o); });
-		std::sort (b, b + 3);
+    std::regex_search (line, m, PARSE);
+    std::transform (++m.begin(), m.end(), b, [] (const auto &o) { return std::stoi (o); });
+    std::sort (b, b + 3);
     total += ((part2) ? (2 * (b[0] + b[1]) + (b[0] * b[1] * b[2])) : (3 * (b[0] * b[1]) + 2 * b[2] * (b[0] + b[1])));
   }
   std::cout << total << std::endl;
