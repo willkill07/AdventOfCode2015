c80f8585eaa18694757d607e95853e9590c08563
Will Killian <william.killian@gmail.com>
2015-12-08 19:48:12 -0500

    golfing on problem 2

--- src/day2.cpp
+++ src/day2.cpp
@@ -9,20 +9,21 @@ static const std::regex PARSE { "(\\d+)x(\\d+)x(\\d+)" };
 
 using Box = std::array <int, 3>;
 
-Box parseLine (std::string line) {
-  std::smatch fields;
-  std::regex_search (line, fields, PARSE);
-  return Box { { std::stoi (fields [1]), std::stoi (fields [2]), std::stoi (fields [3]) } };
+void parseLine (std::string line, Box &b) {
+  std::smatch m;
+  std::regex_search (line, m, PARSE);
+	std::transform (++m.begin(), m.end(), b.begin(), [] (const auto &o) { return std::stoi (o); });
+	std::sort (b.begin(), b.end());
 }
 
 int main (int argc, char* argv []) {
   Timer t;
   bool part2 { argc == 2 };
   int total { 0 };
-  std::string line;
+	Box b;
+	std::string line;
   while (std::getline (std::cin, line)) {
-    Box b { parseLine (line) };
-    std::sort (std::begin (b), std::end (b));
+		parseLine (line, b);
     total += ((part2) ? (2 * (b[0] + b[1]) + (b[0] * b[1] * b[2])) : (3 * (b[0] * b[1]) + 2 * b[2] * (b[0] + b[1])));
   }
   std::cout << total << std::endl;
