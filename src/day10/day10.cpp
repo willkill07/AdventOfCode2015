805cdac873509184855b4263da66ae97c6a01743
Will Killian <william.killian@gmail.com>
2015-12-10 04:28:55 -0500

    Switched to using array instead of string

--- src/day10/day10.cpp
+++ src/day10/day10.cpp
@@ -2,21 +2,23 @@
 #include <functional>
 #include <iostream>
 #include <string>
-#include <sstream>
+#include <vector>
 #include "timer.hpp"
 
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-  std::string s;
-  std::cin >> s;
+  std::string t;
+  std::cin >> t;
+  std::vector <char> s;
+  std::transform (std::begin (t), std::end (t), std::back_inserter (s), std::bind (std::minus <char> { }, std::placeholders::_1, '0'));
   for (int i { 0 }; i < (part2 ? 50 : 40); ++i) {
-    std::ostringstream o;
+    std::vector <char> o;
     for (auto c = std::begin (s); c != std::end (s); ) {
-      auto loc = std::find_if (c, std::end (s), std::bind (std::not_equal_to <char> { }, *c, std::placeholders::_1));
-      o << (loc - c) << *c;
-      c = loc;
+      auto l = std::find_if (c, std::end (s), std::bind (std::not_equal_to <char> { }, *c, std::placeholders::_1));
+      o.push_back (l - c); o.push_back (*c);
+      c = l;
     }
-    s = o.str();
+    s = o;
   }
   std::cout << s.size() << std::endl;
   return 0;
