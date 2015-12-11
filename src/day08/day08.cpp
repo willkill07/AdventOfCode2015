6f158e495a35694509ab43c2534ed0637813e931
Will Killian <william.killian@gmail.com>
2015-12-10 19:21:02 -0500

    Reduced size more

--- src/day08/day08.cpp
+++ src/day08/day08.cpp
@@ -8,21 +8,20 @@
 
 static const std::regex REDUCE { R"(\\(\\|\"|x[0-9a-f]{2}))" }, EXPAND { R"(\"|\\)" };
 
-auto fn (bool part2) -> std::function <int (int, const std::string & s)> {
-  if (!part2)
-    return [] (int c, const auto &s) {
+struct Str {
+  bool second;
+  Str (bool p2) : second { p2 } { }
+  int operator() (int c, const std::string &s) {
+    if (!second)
       return c + std::accumulate (io::re_search (s, REDUCE), { }, 2, [](int v, auto &m) -> int {
           return v + m.length() - 1;
         });
-    };
-  else
-    return [] (int c, const auto &s) {
-      return c + 2 + std::distance (io::re_search (s, EXPAND), { });
-    };
-}
+    return c + 2 + std::distance (io::re_search (s, EXPAND), { });
+  }
+};
 
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-  std::cout << std::accumulate (io::as<std::string> (std::cin), { }, 0, fn (part2)) << std::endl;
+  std::cout << std::accumulate (io::as<std::string> (std::cin), { }, 0, Str { part2 }) << std::endl;
   return 0;
 }
