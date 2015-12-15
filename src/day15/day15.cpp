3141ab16729dcc41918ad2097c24e272a49d66e5
Will Killian <william.killian@gmail.com>
2015-12-15 14:08:33 -0500

    Day 15

--- src/day15/day15.cpp
+++ src/day15/day15.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
+#include <numeric>
 #include <regex>
-#include <string>
 #include <valarray>
 #include <vector>
 #include "timer.hpp"
 #include "io.hpp"
 
-using Ingredient = std::valarray <int>;
-enum Type { SUGAR = 0, SPRINKLES = 1, CANDY = 2, CHOCOLATE = 3, CALORIES = 4 };
+const int FEATURES = 4;
+const int TOTAL_TABLESPOONS = 100;
+
+struct Ingredient {
+  std::valarray <int> data; int calories;
+  explicit Ingredient () : data (FEATURES), calories { 0 } { }
+  Ingredient (const std::valarray <int> & d, int c) : data { d }, calories { c } { }
+};
 
 const static std::regex PARSE { R"(\w+: capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+))" };
 
+void comb (int r, int n, int* dest, std::function<void()> doNext) {
+  if (r == 1)
+    *dest = n, doNext ();
+  else
+    for (int i { 0 }; i <= n; ++i)
+      *dest = i, comb (r - 1, n - i, dest + 1, doNext);
+}
+
+void for_all (int r, int n, std::function <void (int*)> f) {
+  int dest [FEATURES];
+  comb (r, n, dest, [&] {
+    f (dest);
+  });
+}
+
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-  std::vector <Ingredient> d;
+  int count { 0 }, max { 0 };
+  std::vector <Ingredient> ingredients;
   for (const auto & line : io::by_line { std::cin }) {
     std::smatch m { io::regex_parse (line, PARSE) };
-    d.push_back ({ std::stoi (m[1]), std::stoi (m[2]), std::stoi (m[3]), std::stoi (m[4]), std::stoi (m[5]) });
+    ingredients.emplace_back (std::valarray <int> { std::stoi (m[1]), std::stoi (m[2]), std::stoi (m[3]), std::stoi (m[4]) }, std::stoi (m[5]));
+    ++count;
   }
-  uint64_t max { 0 };
-  for (int u { 0 }; u <= 100; ++u)
-    for (int s { 0 }; u + s <= 100; ++s)
-      for (int c { 0 }; u + s + c <= 100; ++c)
-        for (int o { 0 }; u + s + c + o <= 100; ++o)
-          if (u + s + c + o == 100 ) {
-            const auto res = (d [SUGAR]*u + d[SPRINKLES]*s + d[CANDY]*c + d[CHOCOLATE]*o);
-            if (!part2 || res[CALORIES] == 500) {
-              uint64_t prod = 1;
-              for (int x { 0 }; x < 4; ++x)
-                prod *= std::max (0, res [x]);
-              max = std::max (max, prod);
-            }
-          }
+  for_all (count, TOTAL_TABLESPOONS, [&] (int* counts) {
+    Ingredient res;
+    for (int i { 0 }; i < count; ++i)
+      res.data += counts[i] * ingredients[i].data, res.calories += counts[i] * ingredients[i].calories;
+    if (!part2 || res.calories == 500)
+      max = std::max (max, std::accumulate (std::begin (res.data), std::end (res.data), 1, [] (int p, int v) { return p * std::max (v, 0); }));
+  });
   std::cout << max << std::endl;
   return 0;
 }
