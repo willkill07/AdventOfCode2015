1125b6b174cd563fa18509b7e992a44a5926b246
Will Killian <william.killian@gmail.com>
2015-12-17 13:54:06 -0500

    Dynamic programming for both parts! (Day 17)

--- src/day17/day17.cpp
+++ src/day17/day17.cpp
@@ -1,38 +1,35 @@
+#include <array>
 #include <algorithm>
 #include <iostream>
-#include <iterator>
 #include <numeric>
 #include <vector>
-#include <utility>
 #include "timer.hpp"
 #include "io.hpp"
 
 const int TARGET { 150 };
 
-int count (const std::vector <int> & c) {
-  std::vector <int> dp (TARGET + 1); dp [0] = 1;
-  for (int n : c)
-    for (int i { TARGET }; i >= n;  dp [i] += dp [i - n], --i);
-  return dp [TARGET];
-}
-
-int min (const std::vector <int> & c) {
-  int res { 0 }, size { (int)c.size() };
-  static std::vector <int> b (size);
-  for (int k { 1 }; k <= size; ++k) {
-    std::fill (std::begin (b), std::end (b), 0), std::fill (std::begin (b), std::begin (b) + k, 1);
-    do {
-      res += (!std::inner_product (std::begin (c), std::end (c), std::begin (b), -TARGET));
-    } while (std::prev_permutation (std::begin (b), std::end (b)));
-    if (res) break;
-  }
-  return res;
-}
-
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-  std::vector <int> c;
-  std::copy (std::istream_iterator <int> { std::cin }, { }, std::back_inserter (c));
-  std::cout << (part2 ? min (c) : count (c)) << std::endl;
+  std::vector <int> con;
+  std::copy (std::istream_iterator <int> { std::cin }, { }, std::back_inserter (con));
+  std::sort (con.rbegin(), con.rend());
+  int size { (int)con.size() };
+
+  std::array <std::vector <int>, TARGET + 1> sums;
+  for (auto & s : sums)
+    s.resize (size);
+  sums [0][0] = 1;
+
+  for (int c : con)
+    for (int i { TARGET - c }; i >= 0; --i)
+      for (int j { 0 }; j < size; ++j)
+        sums[i + c][j + 1] += sums[i][j];
+
+  int sum { 0 };
+  for (int v : sums.back())
+    if (sum += v, part2 && v != 0 && (sum = v))
+      break;
+
+  std::cout << sum << std::endl;
   return 0;
 }
