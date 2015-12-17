23d0d96568866bc1b3ad807e7eb7b5b8b62b7175
Will Killian <william.killian@gmail.com>
2015-12-17 12:46:32 -0500

    Golf with speed for day 1

--- src/day01/day01.cpp
+++ src/day01/day01.cpp
@@ -4,18 +4,9 @@
 
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-  int ud [2] { 0, 0 };
-  char c;
-  if (!part2) {
-    for (int count { 1 }; std::cin >> c; ++count, ++ud [c - '(']);
-    std::cout << (ud[0] - ud[1]) << std::endl;
-  } else {
-    for (int count { 1 }; std::cin >> c; ++count) {
-      if (++ud [c - '('], ud[0] < ud[1]) {
-        std::cout << count << std::endl;
-        break;
-      }
-    }
-  }
+  int ud [2] { 0, 0 }, count { 0 };
+  for (char c; (!part2 || ud[0] >= ud[1]) && std::cin >> c; ++count)
+    ++ud [c - '('];
+  std::cout << (!part2 ? ud[0] - ud[1] : count) << std::endl;
   return 0;
 }
