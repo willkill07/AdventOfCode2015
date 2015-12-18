d7071ada8133ba5a3d3deaac1304d8f393129228
Will Killian <william.killian@gmail.com>
2015-12-17 19:22:15 -0500

    Dynamic programming -- only two arrays (day 17)

--- src/day17/day17.cpp
+++ src/day17/day17.cpp
@@ -1,7 +1,7 @@
 #include <array>
 #include <algorithm>
 #include <iostream>
-#include <numeric>
+#include <limits>
 #include <vector>
 #include "timer.hpp"
 #include "io.hpp"
@@ -11,25 +11,20 @@ const int TARGET { 150 };
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
   std::vector <int> con;
+  std::array <int, TARGET + 1> best, ways;
+  std::fill (std::begin (best), std::end (best), std::numeric_limits <int>::max() - 1);
   std::copy (std::istream_iterator <int> { std::cin }, { }, std::back_inserter (con));
   std::sort (con.rbegin(), con.rend());
-  int size { (int)con.size() };
-
-  std::array <std::vector <int>, TARGET + 1> sums;
-  for (auto & s : sums)
-    s.resize (size);
-  sums [0][0] = 1;
-
-  for (int c : con)
-    for (int i { TARGET - c }; i >= 0; --i)
-      for (int j { 0 }; j < size; ++j)
-        sums[i + c][j + 1] += sums[i][j];
-
-  int sum { 0 };
-  for (int v : sums.back())
-    if (sum += v, part2 && v != 0 && (sum = v))
-      break;
-
-  std::cout << sum << std::endl;
+  ways[0] = 1, best[0] = 0;
+  for (auto c : con)
+    for (int rem { TARGET }; rem >= c; --rem)
+      if (part2) {
+        if (best[rem - c] + 1 < best[rem])
+          best[rem] = best[rem-c] + 1, ways[rem] = 0;
+        if (best[rem - c] + 1 == best[rem])
+          ways[rem] += ways [rem - c];
+      } else
+        ways[rem] += ways[rem - c];
+  std::cout <<  ways [TARGET] << std::endl;
   return 0;
 }
