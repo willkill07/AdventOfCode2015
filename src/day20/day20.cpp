4a0ac06da719ca132c82af56bcd75bbf84dd2a01
Will Killian <william.killian@gmail.com>
2015-12-22 21:44:16 -0500

    Sped up 18 and 20

--- src/day20/day20.cpp
+++ src/day20/day20.cpp
@@ -1,24 +1,18 @@
+#include <array>
 #include <iostream>
-#include <vector>
 #include "io.hpp"
 #include "timer.hpp"
 
+static std::array <int, 1000000> points;
+
 int main (int argc, char* argv[]) {
   bool part2 { argc == 2 };
-  int sum { std::stoi (io::as_string (std::cin)) };
-  int i { 1 }, low { 1 }, high { 1 }, weight { part2 ? 11 : 10 };
-  while (high * weight <= sum)
-    low *= i, high *= (++i);
-  long size { high - low };
-  std::vector <int> points (size);
-  for (int elf { i }; elf < high; ++elf) {
-    int o { (low % elf) ? elf - low % elf : 0 };
-    for (int h { o }; (part2 ? (h + low) <= (elf * 50) && h < size : h < size); h += elf)
-      points [h] += elf * weight;
-    if (points [o] >= sum) {
-      std::cout << elf << std::endl;
-      break;
-    }
-  }
+  int sum { std::stoi (io::as_string (std::cin)) }, weight { part2 ? 11 : 10 };
+	size_t elf { 1 };
+	for (; elf <= points.size(); ++elf)
+		for (size_t h { elf - 1 }; h < points.size() && (part2 ? h < elf * 50 : true); h += elf)
+			points [h] += elf * weight;
+	for (elf = 0; points [elf++] < sum; );
+	std::cout << elf << std::endl;
   return 0;
 }
