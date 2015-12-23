321b80cda840ce230cf77f3ca99347783d0aeb5c
Will Killian <william.killian@gmail.com>
2015-12-22 22:36:28 -0500

    Improved algorithm for day 4

--- src/day04/day04.cpp
+++ src/day04/day04.cpp
@@ -6,10 +6,10 @@
 
 int main (int argc, char* argv []) {
   bool part2 { argc == 2 };
-  const std::string Z { "000000" }, input { io::as_string (std::cin) };
-  int index { 1 }, off { 0 };
-  while (off = std::mismatch (std::begin (Z), std::end (Z), std::begin (md5 (input + std::to_string (index)))).first - std::begin (Z), (!part2 && off < 5) || (part2 && off < 6))
-    ++index;
+  std::string input { io::as_string (std::cin) };
+  int index { 0 }, off { 0 };
+  while ((part2 && off < 6) || (!part2 && off < 5))
+    off = md5 (input + std::to_string (++index)).find_first_not_of ('0');
   std::cout << index << std::endl;
   return 0;
 }
