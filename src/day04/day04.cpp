6c177fd6fb45d539f2466555ae6b84e4217dc034
Will Killian <william.killian@gmail.com>
2015-12-23 03:02:43 -0500

    Fixed whitespace

--- src/day04/day04.cpp
+++ src/day04/day04.cpp
@@ -7,9 +7,9 @@
 int main (int argc, char* argv []) {
   bool part2 { argc == 2 };
   std::string input { io::as_string (std::cin) };
-	int index { 0 }, off { 0 };
-	while ((part2 && off < 6) || (!part2 && off < 5))
-		off = md5 (input + std::to_string (++index)).find_first_not_of ('0');
-	std::cout << index << std::endl;
+  int index { 0 }, off { 0 };
+  while ((part2 && off < 6) || (!part2 && off < 5))
+    off = md5 (input + std::to_string (++index)).find_first_not_of ('0');
+  std::cout << index << std::endl;
   return 0;
 }
